bad321e39f362edf8fb9f21d24bd141cb1a28f57
sarazwairi <sara.zwairi@gmail.com>
2021-06-14 23:39:01 +0300

    done

--- README.md
+++ README.md
@@ -1,33 +1,35 @@
 # city-explorer-api
 
-# Project Name
 
-**Author**: Your Name Goes Here
+**Author**: SARA ZWAIRI
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+This is an Express API server.
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+Clone this repo to your local machine.
+Open the terminal and run npm i
+Run npm start
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+ExpressJS
+dotenv library
+cors library
 
 ## Change Log
-<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
-
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+app work as API server 
+2pm - 11pm 
 
 ## Credit and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 
-Name of feature: ________________________________
+Name of feature: ________________________________API 
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: _____2h
 
-Start time: _____
+Start time: _____2pm
 
-Finish time: _____
+Finish time: _____11pm
 
-Actual time needed to complete: _____
+Actual time needed to complete: _____9h
--- server.js
+++ server.js
@@ -1,48 +1,37 @@
 'use strict';
 
 require('dotenv').config();
-const express= require('express');
-const weatherData=require('./assets/weather.json')
-const cors=require('cors');
+const express = require('express');
+const weatherData = require('./assets/weather.json')
+const cors = require('cors');
+const e = require('express');
 
-const server=express();
+const server = express();
 server.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3002;
 
-// const PORT=3001;
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server is running on port  ${PORT}`)
 })
-class Forecast {
-    constructor(date,description){
-        this.date=date;
-        this.description=description;
-    }
-}
-server.get('/weather',(req,res)=>{
-    let city=req.query.cityName;
-    let lat=req.query.lat;
-    let long=req.query.lon;
-    let searchQuery=weatherData.find((element)=>{
-        if(city.toLowerCase()==element.city_name.toLowerCase()&& lat==element.lat && long==element.lon)
-            return element;
-})
 
+server.get('/',(req,res)=>{
+  res.send('HOME PAGE') ;
+});
 
-try{
-    let weatherArr=[];
-    let date;
-    let description;
-    let weatherData;
-    for (let i=0;i<searchQuery.data.length;i++){
-        date=searchQuery.data[i].valid_date;
-        description=`low of ${searchQuery.data[i].low_temp},high of ${searchQuery.data[i].max_temp}with ${searchQuery.data[i].weather.description}`;
-        weatherData=new Forecast(date,description);
-        weatherArr.push(weatherData); 
-    }
-    res.send(weatherArr);
-}catch(par){
-    res.send('error')
+server.get('/weather', (req, res) => {
+  let lat=req.query.lat;
+  let lon=req.query.lon;
+  let searchQuery=req.query.searchQuery;
+
+if ( lat==weatherData.lat && lon==weatherData.lon && searchQuery==weatherData.city_name){
+    res.send(weatherData.data);
+}else{
+    res.status(500).send("Something went wrong.");
 }
-    })
+});
+
+//localhost 3002 
+server.get("*", (req, res) => {
+    res.status(404).send("Sorry, this page not found");
+  });
